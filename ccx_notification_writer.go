c01a73b5b3f16a01e8d332612b91bdeb061d27d7
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-04-09 08:05:43 +0200

    Stub for the main module

--- ccx_notification_writer.go
+++ ccx_notification_writer.go
@@ -12,10 +12,56 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Entry point to the notification writer service.
+//
+// The service contains consumer (usually Kafka consumer) that consumes
+// messages from given source, processes those messages and stores them
+// in configured data store.
 package main
 
-import "fmt"
+import (
+	"flag"
+	//"os"
+
+	//"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+const (
+	configFileEnvVariableName = "NOTIFICATION_SERVICE_CONFIG_FILE"
+	defaultConfigFileName     = "config"
+)
+
+// Exit codes
+const (
+	// ExitStatusOK means that the tool finished with success
+	ExitStatusOK = iota
+	// ExitStatusError is a general error code
+	ExitStatusError
+	// ExitStatusConsumerError is returned in case of any consumer-related error
+	ExitStatusConsumerError
+	// ExitStatusStorageError is returned in case of any consumer-related error
+	ExitStatusStorageError
+)
+
+func doSelectedOperation(cliFlags CliFlags) error {
+	return nil
+}
 
 func main() {
-	fmt.Println("vim-go")
+	var cliFlags CliFlags
+
+	// define and parse all command line options
+	flag.BoolVar(&cliFlags.performDatabaseInitialization, "db-init", false, "perform database initialization")
+	flag.BoolVar(&cliFlags.performDatabaseCleanup, "db-clenaup", false, "perform database cleanup")
+	flag.BoolVar(&cliFlags.checkConnectionToKafka, "check-kafka", false, "check connection to Kafka")
+	flag.BoolVar(&cliFlags.showVersion, "version", false, "show cleaner version")
+	flag.BoolVar(&cliFlags.showAuthors, "authors", false, "show authors")
+	flag.Parse()
+
+	// perform selected operation
+	err := doSelectedOperation(cliFlags)
+	if err != nil {
+		log.Err(err).Msg("Operation failed")
+	}
 }
