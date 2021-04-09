cb2531b28be5d5287555c8c34ef8c2c498a062da
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-04-09 10:17:18 +0200

    Database/storage implementation

--- database.go
+++ database.go
@@ -13,3 +13,112 @@
 // limitations under the License.
 
 package main
+
+// This source file contains an implementation of interface between Go code and
+// (almost any) SQL database like PostgreSQL, SQLite, or MariaDB.
+//
+// It is possible to configure connection to selected database by using
+// StorageConfiguration structure. Currently that structure contains two
+// configurable parameter:
+//
+// Driver - a SQL driver, like "sqlite3", "pq" etc.
+// DataSource - specification of data source. The content of this parameter depends on the database used.
+
+import (
+	"fmt"
+
+	"database/sql"
+
+	_ "github.com/lib/pq"           // PostgreSQL database driver
+	_ "github.com/mattn/go-sqlite3" // SQLite database driver
+
+	"github.com/rs/zerolog/log"
+)
+
+// Storage represents an interface to almost any database or storage system
+type Storage interface {
+	Init() error
+	Close() error
+}
+
+// DBStorage is an implementation of Storage interface that use selected SQL like database
+// like SQLite, PostgreSQL, MariaDB, RDS etc. That implementation is based on the standard
+// sql package. It is possible to configure connection via Configuration structure.
+// SQLQueriesLog is log for sql queries, default is nil which means nothing is logged
+type DBStorage struct {
+	connection   *sql.DB
+	dbDriverType DBDriver
+}
+
+// NewStorage function creates and initializes a new instance of Storage interface
+func NewStorage(configuration StorageConfiguration) (*DBStorage, error) {
+	driverType, driverName, dataSource, err := initAndGetDriver(configuration)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Info().Msgf(
+		"Making connection to data storage, driver=%s datasource=%s",
+		driverName, dataSource,
+	)
+
+	connection, err := sql.Open(driverName, dataSource)
+	if err != nil {
+		log.Error().Err(err).Msg("Can not connect to data storage")
+		return nil, err
+	}
+
+	return NewFromConnection(connection, driverType), nil
+}
+
+// NewFromConnection function creates and initializes a new instance of Storage interface from prepared connection
+func NewFromConnection(connection *sql.DB, dbDriverType DBDriver) *DBStorage {
+	return &DBStorage{
+		connection:   connection,
+		dbDriverType: dbDriverType,
+	}
+}
+
+// initAndGetDriver initializes driver(with logs if logSQLQueries is true),
+// checks if it's supported and returns driver type, driver name, dataSource and error
+func initAndGetDriver(configuration StorageConfiguration) (driverType DBDriver, driverName string, dataSource string, err error) {
+	//var driver sql_driver.Driver
+	driverName = configuration.Driver
+
+	switch driverName {
+	case "sqlite3":
+		driverType = DBDriverSQLite3
+		//driver = &sqlite3.SQLiteDriver{}
+		// dataSource = configuration.SQLiteDataSource
+	case "postgres":
+		driverType = DBDriverPostgres
+		//driver = &pq.Driver{}
+		dataSource = fmt.Sprintf(
+			"postgresql://%v:%v@%v:%v/%v?%v",
+			configuration.PGUsername,
+			configuration.PGPassword,
+			configuration.PGHost,
+			configuration.PGPort,
+			configuration.PGDBName,
+			configuration.PGParams,
+		)
+	default:
+		err = fmt.Errorf("driver %v is not supported", driverName)
+		return
+	}
+
+	return
+}
+
+// Close method closes the connection to database. Needs to be called at the end of application lifecycle.
+func (storage DBStorage) Close() error {
+	log.Info().Msg("Closing connection to data storage")
+	if storage.connection != nil {
+		err := storage.connection.Close()
+		if err != nil {
+			log.Error().Err(err).Msg("Can not close connection to data storage")
+			return err
+		}
+	}
+	return nil
+}
