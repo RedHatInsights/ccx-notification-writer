67b7e33d5de7ec3572ed8925b8acccf110b0b627
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-11 10:51:34 +0200

    Link to generated documentation added into ccx_notification_writer_test.go

--- ccx_notification_writer_test.go
+++ ccx_notification_writer_test.go
@@ -16,6 +16,9 @@ limitations under the License.
 
 package main_test
 
+// Documentation in literate-programming-style is available at:
+// https://redhatinsights.github.io/ccx-notification-writer/packages/ccx_notification_writer_test.html
+
 import (
 	"os"
 	"testing"
