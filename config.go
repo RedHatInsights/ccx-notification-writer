e90f50f908fa0b1c9a9dbebc0cc70f9ac07f303f
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-13 08:07:47 +0200

    Added missing description of source file.

--- config.go
+++ config.go
@@ -73,6 +73,7 @@ import (
 	"github.com/spf13/viper"
 )
 
+// Common constants used for during logging and error reporting
 const (
 	filenameAttribute               = "filename"
 	parsingConfigurationFileMessage = "parsing configuration file"
