d8017b74bc94be8ebaaf7f61cf062358c36afc1a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-08-19 10:23:21 +0200

    Minor fix: formatting

--- storage.go
+++ storage.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2021 Red Hat, Inc.
+Copyright © 2021, 2022 Red Hat, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -391,7 +391,6 @@ func NewFromConnection(connection *sql.DB, dbDriverType DBDriver) *DBStorage {
 // initAndGetDriver initializes driver(with logs if logSQLQueries is true),
 // checks if it's supported and returns driver type, driver name, dataSource and error
 func initAndGetDriver(configuration StorageConfiguration) (driverType DBDriver, driverName, dataSource string, err error) {
-	// var driver sql_driver.Driver
 	driverName = configuration.Driver
 
 	switch driverName {
@@ -601,7 +600,7 @@ func (storage DBStorage) DatabaseInitMigration() error {
 	}
 
 	err = func(tx *sql.Tx) error {
-		//check if table already exists
+		// check if table already exists
 		version, err := storage.GetDatabaseVersionInfo()
 
 		if version >= 0 && err == nil {
