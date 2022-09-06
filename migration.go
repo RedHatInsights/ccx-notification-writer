88476450f29ebc0b65607e13b43374faa88d5467
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-09-06 09:29:38 +0200

    Add last migration

--- migration.go
+++ migration.go
@@ -33,6 +33,7 @@ var migrations = []utils.Migration{
 	mig0001CreateEventTargetsTbl,
 	mig0002AddEventTargetCol,
 	mig0003PopulateEventTables,
+	mig0004PopulateEventTables,
 }
 
 // All returns "migration" , the list of implemented utils.Migration
