86ec0c96a1cb4be34576d364bc913d4dc1e4d5de
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-09-07 14:13:43 +0200

    Indexes creation commands

--- db_benchmark_test.go
+++ db_benchmark_test.go
@@ -105,6 +105,63 @@ const (
 			references event_targets(id)
                 );`
 
+	// Index for the reported table used in benchmarks for
+	// notified_at column
+	createIndexReportedNotifiedAtDescV1 = `
+                CREATE INDEX IF NOT EXISTS notified_at_desc_idx
+		    ON reported_benchmark_1
+		 USING btree (notified_at DESC);
+        `
+
+	// Index for the reported table used in benchmarks for
+	// notified_at column
+	createIndexReportedNotifiedAtDescV2 = `
+                CREATE INDEX IF NOT EXISTS notified_at_desc_idx
+		    ON reported_benchmark_2
+		 USING btree (notified_at DESC);
+        `
+
+	// Index for the reported table for updated_at_desc column
+	createIndexReportedUpdatedAtAscV1 = `
+                CREATE INDEX IF NOT EXISTS updated_at_desc_idx
+		    ON reported_benchmark_1
+		 USING btree (updated_at ASC);
+        `
+
+	// Index for the reported table for updated_at_desc column
+	createIndexReportedUpdatedAtAscV2 = `
+                CREATE INDEX IF NOT EXISTS updated_at_desc_idx
+		    ON reported_benchmark_2
+		 USING btree (updated_at ASC);
+        `
+
+	// Optional index for the reported table for event_type_id
+	// column. Index type is set to BTree.
+	// (https://www.postgresql.org/docs/current/indexes-types.html for more info)
+	createIndexReportedV2EventTypeBtree = `
+                CREATE INDEX IF NOT EXISTS event_type_idx
+		    ON reported_benchmark_2
+		 USING btree (event_type_id ASC);
+        `
+
+	// Optional index for the reported table for event_type_id
+	// column. Index type is set to hash.
+	// (https://www.postgresql.org/docs/current/indexes-types.html for more info)
+	createIndexReportedV2EventTypeHash = `
+                CREATE INDEX IF NOT EXISTS event_type_idx
+		    ON reported_benchmark_2
+		 USING hash (event_type_id ASC);
+        `
+
+	// Optional index for the reported table
+	// column. Index type is set to BRIN.
+	// (https://www.postgresql.org/docs/current/indexes-types.html for more info)
+	createIndexReportedV2EventTypeBrin = `
+                CREATE INDEX IF NOT EXISTS event_type_idx
+		    ON reported_benchmark_2
+		 USING brin (event_type_id ASC);
+        `
+
 	insertIntoReportedV1Statement = `
             INSERT INTO reported_benchmark_1
             (org_id, account_number, cluster, notification_type, state, report, updated_at, notified_at, error_log)
