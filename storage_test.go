23e8510fb643525d22fef0ae10dff52a35f15338
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-10-17 09:47:46 +0200

    Unit test for method Storage.Connection

--- storage_test.go
+++ storage_test.go
@@ -862,3 +862,24 @@ func TestClose(t *testing.T) {
 	// it should not fail
 	assert.Nil(t, err)
 }
+
+// TestConnection function checks the method Storage.Connection.
+func TestConnection(t *testing.T) {
+	storage, err := main.NewStorage(main.StorageConfiguration{
+		Driver:        "sqlite3",
+		LogSQLQueries: false,
+	})
+
+	// we just happen to make connection without trying to actually connect
+	assert.Nil(t, err)
+
+	// try to retrieve connection
+	returned := storage.Connection()
+	assert.NotNil(t, returned)
+
+	// try to close the storage
+	err = storage.Close()
+
+	// it should not fail
+	assert.Nil(t, err)
+}
