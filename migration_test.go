fea58aa1560f75d7088c9c284c4b2237a8299cd3
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-10-17 08:12:48 +0200

    Check that migration migration_test_.go fail gracefuly

--- migration_test.go
+++ migration_test.go
@@ -287,6 +287,46 @@ func Test0004MigrationStepUp(t *testing.T) {
 	checkAllExpectations(t, mock)
 }
 
+// Test the 4th migration in case the table update fails.
+func Test0004MigrationStepUp2(t *testing.T) {
+	// error to be thrown
+	mockedError := errors.New("mocked error")
+
+	// prepare new mocked connection to database
+	connection, mock := mustCreateMockConnection(t)
+
+	// prepare mocked result for SQL query
+	rows := sqlmock.NewRows([]string{"version"})
+	rows.AddRow("3")
+
+	count := sqlmock.NewRows([]string{"count"})
+	count.AddRow("1")
+
+	// expected query performed by tested function
+	expectedQuery0 := "SELECT COUNT\\(\\*\\) FROM migration_info;"
+	expectedQuery1 := "SELECT version FROM migration_info;"
+	expectedUpdate1 := "UPDATE reported SET event_type_id = 1 WHERE event_type_id IS NULL"
+
+	mock.ExpectQuery(expectedQuery0).WillReturnRows(count)
+	mock.ExpectQuery(expectedQuery1).WillReturnRows(rows)
+	mock.ExpectBegin()
+
+	// table update will fail
+	mock.ExpectExec(expectedUpdate1).WillReturnError(mockedError)
+
+	// so we expect roll back instead of transaction commit
+	mock.ExpectRollback()
+	mock.ExpectClose()
+
+	utils.Set(main.All())
+
+	// migration should end with error
+	assert.Error(t, main.Migrate(connection, 4), mockedError)
+
+	// check if all expectations were met
+	checkAllExpectations(t, mock)
+}
+
 func Test0004MigrationStepDown(t *testing.T) {
 	// prepare new mocked connection to database
 	connection, mock := mustCreateMockConnection(t)
