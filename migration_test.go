a78544c7ed0bf60eaa8afa8242e3b1eed788e47d
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-02-07 08:18:40 +0100

    Migration 0007 negative step-up unit test

--- migration_test.go
+++ migration_test.go
@@ -1168,6 +1168,48 @@ func Test0007MigrationStepUp(t *testing.T) {
 	checkAllExpectations(t, mock)
 }
 
+// Test0007MigrationStepUpOnMigrationFailure test checks migration #7 in case
+// the migration fails.
+func Test0007MigrationStepUpOnMigrationFailure(t *testing.T) {
+	// error to be thrown
+	mockedError := errors.New("mocked error")
+
+	// prepare new mocked connection to database
+	connection, mock := mustCreateMockConnection(t)
+
+	// prepare mocked result for SQL query
+	rows := sqlmock.NewRows([]string{"version"})
+	rows.AddRow("6")
+
+	count := sqlmock.NewRows([]string{"count"})
+	count.AddRow("1")
+
+	// expected query performed by tested function
+	expectedQuery0 := "SELECT COUNT\\(\\*\\) FROM migration_info;"
+	expectedQuery1 := "SELECT version FROM migration_info;"
+	expectedStatement := "COMMENT ON TABLE event_targets IS 'specification of all event targets currently supported';"
+
+	// queries to retrieve DB version should succeed
+	mock.ExpectQuery(expectedQuery0).WillReturnRows(count)
+	mock.ExpectQuery(expectedQuery1).WillReturnRows(rows)
+	mock.ExpectBegin()
+
+	// statement to change comment will fail
+	mock.ExpectExec(expectedStatement).WillReturnError(mockedError)
+
+	// so we expect roll back instead of transaction commit
+	mock.ExpectRollback()
+	mock.ExpectClose()
+
+	utils.Set(main.All())
+
+	// migrate to version 7
+	assert.Error(t, main.Migrate(connection, 7), mockedError)
+
+	// check if all expectations were met
+	checkAllExpectations(t, mock)
+}
+
 // Test0007MigrationStepDown test checks migration #7, step down part.
 func Test0007MigrationStepDown(t *testing.T) {
 	// prepare new mocked connection to database
