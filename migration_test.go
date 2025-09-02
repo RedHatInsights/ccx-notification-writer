e063f3a04ad074d591cfd0b8b2dd1b071f6d6d09
Juan Diaz Suarez <jdiazsua@redhat.com>
2025-09-02 15:12:29 +0200

    test: add unit tests for migration 8

--- migration_test.go
+++ migration_test.go
@@ -1330,3 +1330,228 @@ func Test0007MigrationStepDownOnMigrationFailure(t *testing.T) {
 	// check if all expectations were met
 	checkAllExpectations(t, mock)
 }
+
+// Test0008MigrationStepUp test checks migration #8, step up part.
+func Test0008MigrationStepUp(t *testing.T) {
+	// prepare new mocked connection to database
+	connection, mock := mustCreateMockConnection(t)
+
+	// prepare mocked result for SQL query
+	rows := sqlmock.NewRows([]string{"version"})
+	rows.AddRow("7")
+
+	count := sqlmock.NewRows([]string{"count"})
+	count.AddRow("1")
+
+	resultCreate1 := sqlmock.NewResult(0, 1)
+	resultCreate2 := sqlmock.NewResult(0, 1)
+	resultUpdate := sqlmock.NewResult(1, 1)
+
+	// expected query performed by tested function
+	expectedQuery0 := "SELECT COUNT\\(\\*\\) FROM migration_info;"
+	expectedQuery1 := "SELECT version FROM migration_info;"
+	expectedCreate1 := "CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_reported_event_state_org_cluster ON reported\\(event_type_id, state, org_id, cluster\\);"
+	expectedCreate2 := "CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_reported_updated_at ON reported\\(updated_at\\);"
+	expectedUpdate := "UPDATE migration_info SET version=\\$1;"
+
+	mock.ExpectQuery(expectedQuery0).WillReturnRows(count)
+	mock.ExpectQuery(expectedQuery1).WillReturnRows(rows)
+	mock.ExpectBegin()
+	mock.ExpectExec(expectedCreate1).WillReturnResult(resultCreate1)
+	mock.ExpectExec(expectedCreate2).WillReturnResult(resultCreate2)
+	mock.ExpectExec(expectedUpdate).WillReturnResult(resultUpdate)
+	mock.ExpectCommit()
+	mock.ExpectClose()
+
+	// prepare list of all migrations
+	utils.Set(main.All())
+
+	// migrate to version 8
+	assert.NoError(t, main.Migrate(connection, 8))
+
+	// check if all expectations were met
+	checkAllExpectations(t, mock)
+}
+
+// Test0008MigrationStepUpOnMigrationFailure1 test checks migration #8 in case
+// the first index creation fails.
+func Test0008MigrationStepUpOnMigrationFailure1(t *testing.T) {
+	// error to be thrown
+	mockedError := errors.New("mocked error")
+
+	// prepare new mocked connection to database
+	connection, mock := mustCreateMockConnection(t)
+
+	// prepare mocked result for SQL query
+	rows := sqlmock.NewRows([]string{"version"})
+	rows.AddRow("7")
+
+	count := sqlmock.NewRows([]string{"count"})
+	count.AddRow("1")
+
+	// expected query performed by tested function
+	expectedQuery0 := "SELECT COUNT\\(\\*\\) FROM migration_info;"
+	expectedQuery1 := "SELECT version FROM migration_info;"
+	expectedCreate1 := "CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_reported_event_state_org_cluster ON reported\\(event_type_id, state, org_id, cluster\\);"
+
+	// queries to retrieve DB version should succeed
+	mock.ExpectQuery(expectedQuery0).WillReturnRows(count)
+	mock.ExpectQuery(expectedQuery1).WillReturnRows(rows)
+	mock.ExpectBegin()
+
+	// first index creation will fail
+	mock.ExpectExec(expectedCreate1).WillReturnError(mockedError)
+
+	// so we expect roll back instead of transaction commit
+	mock.ExpectRollback()
+	mock.ExpectClose()
+
+	// prepare list of all migrations
+	utils.Set(main.All())
+
+	// migrate to version 8
+	assert.Error(t, main.Migrate(connection, 8), mockedError)
+
+	// check if all expectations were met
+	checkAllExpectations(t, mock)
+}
+
+// Test0008MigrationStepUpOnMigrationFailure2 test checks migration #8 in case
+// the second index creation fails.
+func Test0008MigrationStepUpOnMigrationFailure2(t *testing.T) {
+	// error to be thrown
+	mockedError := errors.New("mocked error")
+
+	// prepare new mocked connection to database
+	connection, mock := mustCreateMockConnection(t)
+
+	// prepare mocked result for SQL query
+	rows := sqlmock.NewRows([]string{"version"})
+	rows.AddRow("7")
+
+	count := sqlmock.NewRows([]string{"count"})
+	count.AddRow("1")
+
+	resultCreate1 := sqlmock.NewResult(0, 1)
+
+	// expected query performed by tested function
+	expectedQuery0 := "SELECT COUNT\\(\\*\\) FROM migration_info;"
+	expectedQuery1 := "SELECT version FROM migration_info;"
+	expectedCreate1 := "CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_reported_event_state_org_cluster ON reported\\(event_type_id, state, org_id, cluster\\);"
+	expectedCreate2 := "CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_reported_updated_at ON reported\\(updated_at\\);"
+
+	// queries to retrieve DB version should succeed
+	mock.ExpectQuery(expectedQuery0).WillReturnRows(count)
+	mock.ExpectQuery(expectedQuery1).WillReturnRows(rows)
+	mock.ExpectBegin()
+
+	// first index creation will succeed
+	mock.ExpectExec(expectedCreate1).WillReturnResult(resultCreate1)
+
+	// second index creation will fail
+	mock.ExpectExec(expectedCreate2).WillReturnError(mockedError)
+
+	// so we expect roll back instead of transaction commit
+	mock.ExpectRollback()
+	mock.ExpectClose()
+
+	// prepare list of all migrations
+	utils.Set(main.All())
+
+	// migrate to version 8
+	assert.Error(t, main.Migrate(connection, 8), mockedError)
+
+	// check if all expectations were met
+	checkAllExpectations(t, mock)
+}
+
+// Test0008MigrationStepDown test checks migration #8, step down part.
+func Test0008MigrationStepDown(t *testing.T) {
+	// prepare new mocked connection to database
+	connection, mock := mustCreateMockConnection(t)
+
+	// prepare mocked result for SQL query
+	rows := sqlmock.NewRows([]string{"version"})
+	rows.AddRow("8")
+
+	count := sqlmock.NewRows([]string{"count"})
+	count.AddRow("1")
+
+	resultDrop1 := sqlmock.NewResult(0, 1)
+	resultDrop2 := sqlmock.NewResult(0, 1)
+	resultUpdate := sqlmock.NewResult(1, 1)
+
+	// expected query performed by tested function
+	expectedQuery0 := "SELECT COUNT\\(\\*\\) FROM migration_info;"
+	expectedQuery1 := "SELECT version FROM migration_info;"
+	expectedDrop1 := "DROP INDEX IF EXISTS idx_reported_updated_at;"
+	expectedDrop2 := "DROP INDEX IF EXISTS idx_reported_event_state_org_cluster;"
+	expectedUpdate := "UPDATE migration_info SET version=\\$1;"
+
+	mock.ExpectQuery(expectedQuery0).WillReturnRows(count)
+	mock.ExpectQuery(expectedQuery1).WillReturnRows(rows)
+	mock.ExpectBegin()
+	mock.ExpectExec(expectedDrop1).WillReturnResult(resultDrop1)
+	mock.ExpectExec(expectedDrop2).WillReturnResult(resultDrop2)
+	mock.ExpectExec(expectedUpdate).WillReturnResult(resultUpdate)
+	mock.ExpectCommit()
+	mock.ExpectClose()
+
+	// prepare list of all migrations
+	utils.Set(main.All())
+
+	// migrate to version 7
+	assert.NoError(t, main.Migrate(connection, 7))
+
+	// check if all expectations were met
+	checkAllExpectations(t, mock)
+}
+
+// Test0008MigrationStepDownOnMigrationFailure test checks migration #8 in case
+// the index drop fails.
+func Test0008MigrationStepDownOnMigrationFailure(t *testing.T) {
+	// error to be thrown
+	mockedError := errors.New("mocked error")
+
+	// prepare new mocked connection to database
+	connection, mock := mustCreateMockConnection(t)
+
+	// prepare mocked result for SQL query
+	rows := sqlmock.NewRows([]string{"version"})
+	rows.AddRow("8")
+
+	count := sqlmock.NewRows([]string{"count"})
+	count.AddRow("1")
+
+	resultDrop1 := sqlmock.NewResult(0, 1)
+
+	// expected query performed by tested function
+	expectedQuery0 := "SELECT COUNT\\(\\*\\) FROM migration_info;"
+	expectedQuery1 := "SELECT version FROM migration_info;"
+	expectedDrop1 := "DROP INDEX IF EXISTS idx_reported_updated_at;"
+	expectedDrop2 := "DROP INDEX IF EXISTS idx_reported_event_state_org_cluster;"
+
+	// queries to retrieve DB version should succeed
+	mock.ExpectQuery(expectedQuery0).WillReturnRows(count)
+	mock.ExpectQuery(expectedQuery1).WillReturnRows(rows)
+	mock.ExpectBegin()
+
+	// first index drop will succeed
+	mock.ExpectExec(expectedDrop1).WillReturnResult(resultDrop1)
+
+	// second index drop will fail
+	mock.ExpectExec(expectedDrop2).WillReturnError(mockedError)
+
+	// so we expect roll back instead of transaction commit
+	mock.ExpectRollback()
+	mock.ExpectClose()
+
+	// prepare list of all migrations
+	utils.Set(main.All())
+
+	// migrate to version 7
+	assert.Error(t, main.Migrate(connection, 7), mockedError)
+
+	// check if all expectations were met
+	checkAllExpectations(t, mock)
+}
